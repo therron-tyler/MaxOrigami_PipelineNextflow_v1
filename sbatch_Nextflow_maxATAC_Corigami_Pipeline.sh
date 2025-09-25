90061444a2d783529c22351e8f4e2912fa798133
therron-tyler <therrontyler@gmail.com>
2025-09-25 09:04:06 -0500

    Update sbatch_Nextflow_maxATAC_Corigami_Pipeline.sh

--- sbatch_Nextflow_maxATAC_Corigami_Pipeline.sh
+++ sbatch_Nextflow_maxATAC_Corigami_Pipeline.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -A b1042
-#SBATCH -p genomics
+#SBATCH -A allocation
+#SBATCH -p partition
 #SBATCH -t 40:00:00
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=tyler.therron@northwestern.edu
+#SBATCH --mail-user=user@domain
 #SBATCH --output=%x.%j.out
 #SBATCH --mem=60gb
 #SBATCH --job-name=maxATAC_Corigami_nf_test
@@ -12,10 +12,10 @@
 
 module load nextflow/25.04.0
 
-cd /home/ttm3567/rootdir_scratch/20250806_Corigami/maxATAC_Corigami_NextflowPipeline
+cd <maxATAC_Corigami_NextflowPipeline Directory>
 
 nextflow run TAD_PredictionPipeline_NextflowMain_v1-1.nf \
   --input_pattern '../BAM/*.bam' \
   --chroms 'chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19' \
-  --outdir BowdishATAC_Ly6cHigh_YoungOld_TNFKO_HiC_Prediction \
+  --outdir <Output Name> \
   --run_benchmark true
